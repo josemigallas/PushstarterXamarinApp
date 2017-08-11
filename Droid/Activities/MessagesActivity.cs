722d08660aa46543791756e7027b6ac2d3cc105c
Jose Miguel Gallas Olmedo <jozan@Joses-MacBook-Pro.local>
2017-08-11 10:53:20 +0200

    initial commit

---
+++ Droid/Activities/MessagesActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PushStarterXamainApp.Droid
+{
+    [Activity(
+        Label = "@string/app_name",
+        MainLauncher = true,
+        Icon = "@mipmap/icon"
+    )]
+    public class MessagesActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            TextView text = FindViewById<TextView>(Resource.Id.text);
+
+            text.Text = "Hello!";
+        }
+    }
+}
+
---
+++ PushStarterXamainApp/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PushStarterXamainApp
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
